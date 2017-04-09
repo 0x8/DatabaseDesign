90a002e921d24c79b96cf40d901b63c34d9af5e5
Kevin Orr <kevinorr54@gmail.com>
2017-04-08 23:07:07 -0400

    Updated query.py for psycopg2 instead of sqlite, and schema change

--- query.py
+++ query.py
@@ -1,64 +1,69 @@
 queries = {
     'table_stores': '''
-        select id as store_id, phone_number, street_address, city, zip_code
+        select sid as store_id, telno, address, city, state, zip
         from Store
-        where city = :city or :city is null
+        where city = %(city) or %(city) is null
     ''',
 
     'table_employees': '''
-        select id as employee_id, first_name, last_name
-        from Employees
-        where (first_name = :first_name or :first_name is null) and
-              (last_name = :last_name or :last_name is null)
+        select eid as employee_id, firstname, lastname, role, pay, hourly
+        from Employee
+        join Role on Employee.roleid = Role.roleid
+        where (firstname = %(first_name) or %(first_name) is null) and
+              (lastname = %(last_name) or %(last_name) is null)
     ''',
 
     'table_products': '''
-        select id as product_id, name
-        from Products
-        where (name = :name or :name is null)
+        select id as product_id, name, color
+        from Product
+        where (name = %(name) or %(name) is null)
     ''',
 
     'relationship_inventory': '''
-        select Product.id as product_id, Product.name as product_name,
-               price, stock,
-               Store.id as store_id, phone_number, street_address, city, zip_code
-        from Product, Inventory, Store
-        where Product.id = Inventory.pid and Inventory.sid = Store.id and
-              ((:price_comparison = "none" or :price_comparison is null) or
-               (:price_comparison = "greater" and price > :price_value)
-               (:price_comparison = "less" and price < :price_value)
-               (:price_comparison = "equal" and price = :price_value)) and
-              ((:amount_comparison = "none" or :amount_comparison is null) or
-               (:amount_comparison = "greater" and amount > :amount_value)
-               (:amount_comparison = "less" and amount < :amount_value)
-               (:amount_comparison = "equal" and amount = :amount_value))
+        select Product.pid as product_id, Product.name as product_name, color
+               price, stock, special
+               Store.sid as store_id, telno, address, city, state, zip
+        from Product
+        join Inventory on Product.pid = Inventory.pid
+        join Store on Inventory.sid = Store.sid
+        where Product.pid = Inventory.pid and Inventory.sid = Store.sid and
+              ((%(price_comparison) = 'none' or %(price_comparison) is null) or
+               (%(price_comparison) = 'greater' and price > %(price_value))
+               (%(price_comparison) = 'less' and price < %(price_value))
+               (%(price_comparison) = 'equal' and price = %(price_value))) and
+              ((%(amount_comparison) = 'none' or %(amount_comparison) is null) or
+               (%(amount_comparison) = 'greater' and amount > %(amount_value))
+               (%(amount_comparison) = 'less' and amount < %(amount_value))
+               (%(amount_comparison) = 'equal' and amount = %(amount_value)))
     ''',
 
-    # TODO @kevorr: I think my schema is different, this likely needs to be modified
     'relationship_employment': '''
-        select Employee.id as employee_id, first_name, last_name
-               role, salary
-               Store.id as store_id, phone_number, street_address, city, zip_code
-        from Employee, Employment, Store
-        where (first_name = :first_name or :first_name is null) and
-              (last_name = :last_name or :last_name is null) and
-              (city = :city or :city is null)
+        select Employee.eid as employee_id, firstname, lastname, role, pay, hourly
+               Store.sid as store_id, telno, telno, city, zip
+        from Employee
+        join Employment on Employee.eid = Employment.eid
+        join Store on Store.sid = Employment.sid
+        where (firstname = %(first_name) or %(first_name) is null) and
+              (lastname = %(last_name) or %(last_name) is null) and
+              (city = %(city) or %(city) is null)
     ''',
 
     'query_store_inventory': '''
-        select Product.id as product_id, Product.name as product_name,
-               price, stock
-        from Product, Inventory, Store
-        where Store.id = :store_id and
-              Product.id = Inventory.pid and Inventory.sid = Store.id and
-              ((:price_comparison = "none" or :price_comparison is null) or
-               (:price_comparison = "greater" and price > :price_value)
-               (:price_comparison = "less" and price < :price_value)
-               (:price_comparison = "equal" and price = :price_value)) and
-              ((:amount_comparison = "none" or :amount_comparison is null) or
-               (:amount_comparison = "greater" and amount > :amount_value)
-               (:amount_comparison = "less" and amount < :amount_value)
-               (:amount_comparison = "equal" and amount = :amount_value))
+        select Product.pid as product_id, Product.name as product_name, color
+               price, stock, special
+        from Product
+        join Inventory on Product.pid = Inventory.pid
+        join Store on Store.sid = Inventory.sid
+        where Store.sid = %(store_id) and
+              Product.pid = Inventory.pid and Inventory.sid = Store.sid and
+              ((%(price_comparison) = 'none' or %(price_comparison) is null) or
+               (%(price_comparison) = 'greater' and price > %(price_value))
+               (%(price_comparison) = 'less' and price < %(price_value))
+               (%(price_comparison) = 'equal' and price = %(price_value))) and
+              ((%(amount_comparison) = 'none' or %(amount_comparison) is null) or
+               (%(amount_comparison) = 'greater' and amount > %(amount_value))
+               (%(amount_comparison) = 'less' and amount < %(amount_value))
+               (%(amount_comparison) = 'equal' and amount = %(amount_value)))
     '''
 
 }
