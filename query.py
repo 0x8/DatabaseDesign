05ff59fb2ed5ca52345ec9685e42b6542ea9d9d1
Kevin Orr <kevinorr54@gmail.com>
2017-04-09 21:59:08 -0400

    Bug squashing in query.py

--- query.py
+++ query.py
@@ -2,50 +2,51 @@ queries = {
     'table_stores': '''
         select sid as store_id, telno, address, city, state, zip
         from Store
-        where city = %(city) or %(city) is null
+        where city = %(city)s or %(city)s is null
     ''',
 
     'table_employees': '''
         select eid as employee_id, firstname, lastname, role, pay, hourly
         from Employee
-        join Role on Employee.roleid = Role.roleid
-        where (firstname = %(first_name) or %(first_name) is null) and
-              (lastname = %(last_name) or %(last_name) is null)
+        join Role on Employee.role_id = Role.role_id
+        where (firstname = %(first_name)s or %(first_name)s is null) and
+              (lastname = %(last_name)s or %(last_name)s is null)
     ''',
 
     'table_products': '''
-        select id as product_id, name, color
+        select pid as product_id, name, color
         from Product
-        where (name = %(name) or %(name) is null)
+        where (name = %(product_name)s or %(product_name)s is null)
     ''',
 
     'relationship_inventory': '''
         select Product.pid as product_id, Product.name as product_name, color
-               price, stock, special
+               price, stock, special,
                Store.sid as store_id, telno, address, city, state, zip
         from Product
         join Inventory on Product.pid = Inventory.pid
         join Store on Inventory.sid = Store.sid
         where Product.pid = Inventory.pid and Inventory.sid = Store.sid and
-              ((%(price_comparison) = 'none' or %(price_comparison) is null) or
-               (%(price_comparison) = 'greater' and price > %(price_value))
-               (%(price_comparison) = 'less' and price < %(price_value))
-               (%(price_comparison) = 'equal' and price = %(price_value))) and
-              ((%(amount_comparison) = 'none' or %(amount_comparison) is null) or
-               (%(amount_comparison) = 'greater' and amount > %(amount_value))
-               (%(amount_comparison) = 'less' and amount < %(amount_value))
-               (%(amount_comparison) = 'equal' and amount = %(amount_value)))
+              ((%(price_comparison)s = 'none' or %(price_comparison)s is null) or
+               (%(price_comparison)s = 'greater' and price > %(price_value)s) or
+               (%(price_comparison)s = 'less' and price < %(price_value)s) or
+               (%(price_comparison)s = 'equal' and price = %(price_value)s)) and
+              ((%(amount_comparison)s = 'none' or %(amount_comparison)s is null) or
+               (%(amount_comparison)s = 'greater' and stock > %(amount_value)s) or
+               (%(amount_comparison)s = 'less' and stock < %(amount_value)s) or
+               (%(amount_comparison)s = 'equal' and stock = %(amount_value)s))
     ''',
 
     'relationship_employment': '''
-        select Employee.eid as employee_id, firstname, lastname, role, pay, hourly
+        select Employee.eid as employee_id, firstname, lastname, role, pay, hourly,
                Store.sid as store_id, telno, telno, city, zip
         from Employee
+        join Role on Employee.role_id = Role.role_id
         join Employment on Employee.eid = Employment.eid
         join Store on Store.sid = Employment.sid
-        where (firstname = %(first_name) or %(first_name) is null) and
-              (lastname = %(last_name) or %(last_name) is null) and
-              (city = %(city) or %(city) is null)
+        where (firstname = %(first_name)s or %(first_name)s is null) and
+              (lastname = %(last_name)s or %(last_name)s is null) and
+              (city = %(city)s or %(city)s is null)
     ''',
 
     'query_store_inventory': '''
@@ -54,16 +55,16 @@ queries = {
         from Product
         join Inventory on Product.pid = Inventory.pid
         join Store on Store.sid = Inventory.sid
-        where Store.sid = %(store_id) and
+        where Store.sid = %(store_id)s and
               Product.pid = Inventory.pid and Inventory.sid = Store.sid and
-              ((%(price_comparison) = 'none' or %(price_comparison) is null) or
-               (%(price_comparison) = 'greater' and price > %(price_value))
-               (%(price_comparison) = 'less' and price < %(price_value))
-               (%(price_comparison) = 'equal' and price = %(price_value))) and
-              ((%(amount_comparison) = 'none' or %(amount_comparison) is null) or
-               (%(amount_comparison) = 'greater' and amount > %(amount_value))
-               (%(amount_comparison) = 'less' and amount < %(amount_value))
-               (%(amount_comparison) = 'equal' and amount = %(amount_value)))
+              ((%(price_comparison)s = 'none' or %(price_comparison)s is null) or
+               (%(price_comparison)s = 'greater' and price > %(price_value)s) or
+               (%(price_comparison)s = 'less' and price < %(price_value)s) or
+               (%(price_comparison)s = 'equal' and price = %(price_value)s)) and
+              ((%(amount_comparison)s = 'none' or %(amount_comparison)s is null) or
+               (%(amount_comparison)s = 'greater' and stock > %(amount_value)s) or
+               (%(amount_comparison)s = 'less' and stock < %(amount_value)s) or
+               (%(amount_comparison)s = 'equal' and stock = %(amount_value)s))
     '''
 
 }
