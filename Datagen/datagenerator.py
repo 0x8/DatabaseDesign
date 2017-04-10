271678ee77b2fcaf807ea0856e4a4137de82201a
Kevin Orr <kevinorr54@gmail.com>
2017-04-10 15:04:51 -0400

    Adapt datagenerator.py to use csv library

--- Datagen/datagenerator.py
+++ Datagen/datagenerator.py
@@ -11,10 +11,11 @@ initializing a DB. This gives some example data to work with for demonstrative
 purposes.
 
 '''
-from passlib.hash import bcrypt.sha_256
+from passlib.hash import bcrypt_sha256
 import random
-import os
-
+import os, os.path
+import csv
+import itertools
 
 '''
 These are the headers that should preced each list of rows in csv form. They
@@ -34,7 +35,18 @@ The only methods needed are the init and the __str__ method which prints the
 rows in csv form. This allows quick and easy csv generation.
 '''
 
-class product:
+class DictLike:
+    def __getitem__(self, obj):
+        print('getting %r' % obj)
+        return self._dict[obj]
+
+    def get(self, obj, default):
+        return self._dict.get(obj, default)
+
+    def __iter__(self):
+        return iter(self._dict)
+
+class product(DictLike):
     '''
     This class holds products. The attributes are:
 
@@ -43,18 +55,14 @@ class product:
     color = color of the product. For our purposes we allow only a single color
             per item
     '''
-    
-    # Set up with the selected items
-    def __init__(self, name, color):
-        self.name = name
-        self.color = color
-    
-    # CSV Representation when print
-    def __str__(self):
-        return "{0},{1}".format(self.name,self.color)
 
-class supplier:
-    
+    fields = ('pid', 'name', 'color')
+
+    # Set up with the selected items
+    def __init__(self, id, name, color):
+        self._dict = {'pid': id, 'name': name, 'color': color}
+
+class supplier(DictLike):
     '''
     This class ties supplier names to their IDs. The attributes are:
 
@@ -62,14 +70,12 @@ class supplier:
     name = name of the supplier
     '''
 
-    def __init__(self, name):
-        self.name = name
+    fields = ('supid', 'name')
 
-    def __str__(self):
-        return "{0}".format(self.name)
+    def __init__(self, id, name):
+        self._dict = {'supid': id, 'name': name}
 
-class store:
-    
+class store(DictLike):
     '''
     This class holds basic information about a store. The attributes are:
 
@@ -81,24 +87,13 @@ class store:
     telno = telephone number of the store
     '''
 
-    def __init__(self, zip, address, city, state, telno):
-        self.zip = zip
-        self.address = address
-        self.city = city
-        self.state = state
-        self.telno = telno
+    fields = ('sid', 'address', 'city', 'state', 'zip', 'telno')
 
-    def __str__(self):
-        return "{0},{1},{2},{3},{4}".format(
-                                        self.zip,
-                                        self.address,
-                                        self.city,
-                                        self.state,
-                                        self.telno
-                                     )
+    def __init__(self, id, zip, address, city, state, telno):
+        self._dict = {'sid': id, 'address': address, 'city': city, 'state': state,
+                      'zip': zip, 'telno': telno}
 
-class inventory:
-    
+class inventory(DictLike):
     '''
     This class contains the inventory information for any given store. It does
     so by mapping store id's to product id's. The attributes are as follows:
@@ -111,76 +106,43 @@ class inventory:
 
     '''
 
-    def __init__(self,sid,pid,price,qty,special):
-        self.sid = sid
-        self.pid = pid
-        self.price = price
-        self.qty = qty
-        self.special = special
+    fields = ('self', 'sid', 'pid', 'price', 'qty', 'special')
 
-    def __str__(self):
-        return "{0},{1},{2},{3},{4}".format(
-                                        self.sid, 
-                                        self.pid, 
-                                        self.price,
-                                        self.qty,
-                                        self.special
-                                     )
+    def __init__(self, sid, pid, price, qty, special):
+        self._dict = {'sid': sid, 'pid': pid, 'price': price, 'qty': qty, 'special': special}
 
-class prod_supplier:
-    
-    def __init__(self,txid,supid,pid,cost,qty):
-        self.supid = supid
-        self.pid = pid
-        self.cost = cost
-        self.qty = qty
+class prod_supplier(DictLike):
+    fields = ('txid', 'supid', 'pid', 'cost', 'qty')
 
-    def __str__(self):
-        return "{0},{1},{2},{3}".format(
-                                        self.supid,
-                                        self.pid,
-                                        self.cost,
-                                        self.qty
-                                     )
-class employment:
+    def __init__(self, txid, supid, pid, cost, qty):
+        self._dict = {'supid': supid, 'pid': pid, 'cost': cost, 'qty': qty}
 
-    def __init__(self,sid,eid):
-        self.sid = sid
-        self.eid = eid
+class employment(DictLike):
+    fields = ('sid', 'eid')
 
-    def __str__(self):
-        return "{0},{1}".format(self.sid,self.eid)
+    def __init__(self, sid, eid):
+        self._dict = {'sid': sid, 'eid': eid}
 
-class employee:
-    
-    def __init__(self,firstname,lastname,pay,hourly):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.pay = pay
-        self.hourly = hourly
+class employee(DictLike):
+    fields = ('eid', 'firstname', 'lastname', 'pay', 'hourly')
 
-    def __str__(self):
-        return "{0},{1},{2},{3}".format(
-                                            self.firstname,
-                                            self.lastname,
-                                            self.pay,
-                                            self.hourly
-                                         )
+    def __init__(self, eid, firstname, lastname, roleid, pay, hourly):
+        self._dict = {'eid': eid, 'firstname': firstname, 'lastname': lastname,
+                      'roleid': roleid, 'pay': pay, 'hourly': hourly}
 
-class order:
-    
-    def __init__(self,sid,pid,num):
-        self.sid = sid
-        self.pid = pid
-        self.num = num
+class role(DictLike):
+    fields = ('roleid', 'role')
 
-    def __str__(self):
-        return "{0},{1},{2}".format(self.sid,
-                                    self.pid,
-                                    self.num)
+    def __init__(self, roleid, role):
+        self._dict = {'roleid': roleid, 'role': role}
 
-class transaction:
+class order(DictLike):
+    fields = ('oid', 'sid', 'pid', 'num', 'cost')
 
+    def __init__(self, sid, pid, num, cost):
+        self._dict = {'sid': sid, 'pid': pid, 'num': num, 'cost': cost}
+
+class transaction(DictLike):
     '''
     This holds records of transactions per store. A good idea for maintaining
     uniqueness here might be to take the cid of the customers table and the
@@ -191,15 +153,25 @@ class transaction:
     amount = total for the transaction
     '''
 
-    def __init__(self,sid,amount):
-        self.sid = sid
-        self.amount = amount
+    def __init__(self, txid, sid, amount):
+        self._dict = {'txid': txid, 'sid': sid, 'amount': amount}
 
-    def __str__(self):
-        return "{0},{1}".format(self.sid,self.amount)
+class user(DictLike):
+    fields = ('uid', 'username', 'password')
+
+    def __init__(self, uid, username, password):
+        self._dict = {'uid': uid, 'username': username, 'password': password}
 
 
 #================================= Generator ==================================#
+
+# Save list of generators as well as which files to write each to
+_file_class_assocs = []
+def saves_to_file(path, return_class):
+    def wrapper(gen):
+        _file_class_assocs.append((path, gen, return_class))
+        return gen
+    return wrapper
 
 # Generate random first name
 def fname_gen():
@@ -218,6 +190,8 @@ def lname_gen():
     lnames = ['Johnson','Smith','Williams','Brown','Jackson','Ming','Zhang',
               'Jefferson','Thomas','Taylor','Moore','Loss','Davis','Garcia',
               'Miller','Jones','Wilson','Less']
+    while True:
+        yield random.choice(lnames)
 
 
 # Generate usernames
@@ -226,9 +200,9 @@ def uname_gen():
     unames = ['dragonslayer','bargainhunter','salessearcher','genericcust1',
               'magnumdragon','shroud','xgod','taz','mark','milkman',
               'masterblaster','thunderous','fireknight','shovelfighter']
-    
+
     while True:
-        yield random.choice(uname) + str(random.randint(10,100))
+        yield random.choice(unames) + str(random.randint(10,100))
 
 
 # Generate passwords
@@ -238,15 +212,19 @@ def pass_gen():
     while True:
         yield bcrypt_sha256.hash(random.choice(passwords))
 
+# Generate Users
+@saves_to_file('./Data/users.csv', user)
+def user_gen():
+    return itertools.starmap(user, zip(itertools.count(1), uname_gen(), pass_gen()))
 
 # Generate random telephone number
-def gen_telno():
-    
+def telno_gen():
     while True:
         # Area code
-        area = str(random.randint(100,1000))   # 100 - 999
-        pre  = str(random.randint(100,1000))   # 100 - 999
-        last = str(random.randint(1000,10000)) # 1000 - 9999
+        # Note: random.randint bounds are inclusive
+        area = str(random.randint(100,999))   # 100 - 999
+        pre  = str(random.randint(100,999))   # 100 - 999
+        last = str(random.randint(1000,9999)) # 1000 - 9999
         telno = area + '-' + pre + '-' + last
         yield telno
 
@@ -282,22 +260,25 @@ def state_gen():
     while True:
         yield random.choice(states)
 
+def zip_gen():
+    while True:
+        yield str(random.randint(10000, 99999))
+
+# Generate stores
+@saves_to_file('./Data/stores.csv', store)
+def gen_stores():
+    return itertools.starmap(store, zip(itertools.count(1), zip_gen(), address_gen(),
+                                        city_gen(), state_gen(), telno_gen()))
 
 # Generate the roles
+@saves_to_file('./Data/roles.csv', role)
 def gen_roles():
-    counter = 1
-    all_roles = []
     # A list of available roles
     roles = ['Cashier','Manager','Stocker','Human Resources',
              'Information Technology']
 
-    for r in roles:
-        r = role(counter,r)
-        counter += 1
-        all_roles.append(r)
+    return itertools.starmap(role, zip(itertools.count(1), roles))
 
-    return all_roles
-    
 # Generate random product name
 def pname_gen():
     # A list of products
@@ -316,50 +297,38 @@ def color_gen():
     while True:
         yield random.choice(colors)
 
+@saves_to_file('./Data/products.csv', product)
+def product_gen():
+    return itertools.starmap(product, zip(itertools.count(1), pname_gen(), color_gen()))
 
 # Generate the actual CSV files
 def gen_rows(n=100):
-    
-    supplier_header = 'name'
+
     store_hearder = 'zip,address,city,telno'
+    product_header = 'name,color'
     inventory_header = 'sid,pid,price,qty,special'
     prod_supplier_header = 'txid,supid,pid,cost,qty'
     employement_header = 'sid,eid'
     employee_header = 'roleid,firstname,lastname,pay,hourly'
-    role_header = 'roleid,role'   
-    
-    # roles.csv
-    with open('./Data/roles.csv','w') as f:
-        all_roles = gen_roles()
-        for r in all_roles:
-            f.write(r)
+    role_header = 'roleid,role'
 
-    # products.csv
-    with open('./Data/products.csv','w') as f:
-        
-        product_header = 'name,color'
-        for i in range(n):
-            pname = next(pname_gen)
-            color = next(color_gen)
-            line = '{0},{1}'.format(pname, color)
+    for path, gen, cls in _file_class_assocs:
+        with open(path, 'w') as f:
+            print(cls.fields)
+            writer = csv.DictWriter(f, fieldnames=cls.fields)
+            writer.writeheader()
 
-    # stores.csv
-    with open('./Data/stores.csv','w') as f:
-        for i in range(n):
-            pass
-
-    # users.csv
-    with open('./Data/users.csv','w') as f:
-        user_header = 'username,password'
-        f.write(user_header)
-        for i in range(n):
-            username = next(uname_gen)
-            password = next(pass_gen)
-            line = '{0},{1}'.format(username,password)
-            f.write(line)
+            # range() here just limits otherwise infinite generators
+            for _, obj in zip(range(n), gen()):
+                print(obj)
+                print(obj._dict)
+                writer.writerow(obj)
 
 # =============== [ Main ] ============== #
 # Create the data directory if it does not exist already
-dir_Create = 'if [ ! -f ./Data ]; then mkdir Data; fi'
-os.system(dir_Create)
+if not os.path.exists('Data'):
+    os.mkdir('Data')
+elif not os.path.isdir('Data'):
+    e = RuntimeError('Path %r exists but is not a directory' % 'Data')
+
 gen_rows(10)
