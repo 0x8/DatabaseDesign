62e81b078dc8d905d81bcdcfd7f552aae6d5b1ae
Kevin Orr <kevinorr54@gmail.com>
2017-04-15 15:58:42 -0400

    [datagen] Completed, minor schema changes may be needed

--- Datagen/datagenerator.py
+++ Datagen/datagenerator.py
@@ -19,167 +19,13 @@ import itertools
 
 from passlib.hash import bcrypt_sha256
 
-'''
-These are the headers that should preced each list of rows in csv form. They
-let postgres know what order the elements are printed in and therefore how to
-insert them into the database appropriately.
-'''
-
 THIS_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
+#================================= Generators =================================#
 
-#================================== Classes ===================================#
-
-'''
-The following are classes to hold each type of item in the database. Each
-instance of one of the following classes represents one row under that table.
-The only methods needed are the init and the __str__ method which prints the
-rows in csv form. This allows quick and easy csv generation.
-'''
-
-class DictLike:
-    def __iter__(self):
-        return iter(self._dict)
-
-    def __getattr__(self, attr):
-        return getattr(self._dict, attr)
-
-# Save dict of class -> string associating object classes to target files
-_file_class_assocs = []
-def saves_to_file(path):
-    def wrapper(cls):
-        _file_class_assocs[cls] = os.path.join(THIS_FILE_PATH, path)
-        return cls
-    return wrapper
-
-@saves_to_file('Data/products.csv')
-class product(DictLike):
-    '''
-    This class holds products. The attributes are:
-
-    pid = product id, unique numeric value representing the product
-    name = product name, what the item is (e.g. 'xbox', 'ps3', 'hammer', 'etc').
-    color = color of the product. For our purposes we allow only a single color
-            per item
-    '''
-
-    fields = ('pid', 'name', 'color')
-
-    # Set up with the selected items
-    def __init__(self, id, name, color):
-        self._dict = {'pid': id, 'name': name, 'color': color}
-
-@saves_to_file('Data/suppliers.csv')
-class supplier(DictLike):
-    '''
-    This class ties supplier names to their IDs. The attributes are:
-
-    supid = supplier id. A numeric representation of who this supplier is
-    name = name of the supplier
-    '''
-
-    fields = ('supid', 'name')
-
-    def __init__(self, id, name):
-        self._dict = {'supid': id, 'name': name}
-
-@saves_to_file('Data/stores.csv')
-class store(DictLike):
-    '''
-    This class holds basic information about a store. The attributes are:
-
-    sid = storeid, unique store identifier
-    zip = store zip code (assumes only US-based stores)
-    address = street address of the store
-    city = city the store is in
-    state = state the store is in
-    telno = telephone number of the store
-    '''
-
-    fields = ('sid', 'address', 'city', 'state', 'zip', 'telno')
-
-    def __init__(self, id, zip, address, city, state, telno):
-        self._dict = {'sid': id, 'address': address, 'city': city, 'state': state,
-                      'zip': zip, 'telno': telno}
-
-@saves_to_file('Data/inventory.csv')
-class inventory(DictLike):
-    '''
-    This class contains the inventory information for any given store. It does
-    so by mapping store id's to product id's. The attributes are as follows:
-
-    self.sid = store id
-    self.pid = product id
-    self.price = price of the product at a given store
-    self.qty = how many of that product a store has
-    self.special = whether the item is on special in a given store
-
-    '''
-
-    fields = ('self', 'sid', 'pid', 'price', 'qty', 'special')
-
-    def __init__(self, sid, pid, price, qty, special):
-        self._dict = {'sid': sid, 'pid': pid, 'price': price, 'qty': qty, 'special': special}
-
-class prod_supplier(DictLike):
-    fields = ('txid', 'supid', 'pid', 'cost', 'qty')
-
-    def __init__(self, txid, supid, pid, cost, qty):
-        self._dict = {'supid': supid, 'pid': pid, 'cost': cost, 'qty': qty}
-
-@saves_to_file('Data/employment.csv')
-class employment(DictLike):
-    fields = ('sid', 'eid')
-
-    def __init__(self, sid, eid):
-        self._dict = {'sid': sid, 'eid': eid}
-
-@saves_to_file('Data/employees.csv')
-class employee(DictLike):
-    fields = ('eid', 'firstname', 'lastname', 'roleid', 'pay', 'hourly')
-
-    def __init__(self, eid, firstname, lastname, roleid, pay, hourly):
-        self._dict = {'eid': eid, 'firstname': firstname, 'lastname': lastname,
-                      'roleid': roleid, 'pay': pay, 'hourly': hourly}
-
-@saves_to_file('Data/roles.csv')
-class role(DictLike):
-    fields = ('roleid', 'role')
-
-    def __init__(self, roleid, role):
-        self._dict = {'roleid': roleid, 'role': role}
-
-@saves_to_file('Data/orders.csv')
-class order(DictLike):
-    fields = ('oid', 'sid', 'pid', 'num', 'cost')
-
-    def __init__(self, sid, pid, num, cost):
-        self._dict = {'sid': sid, 'pid': pid, 'num': num, 'cost': cost}
-
-@saves_to_file('Data/transactions.csv')
-class transaction(DictLike):
-    '''
-    This holds records of transactions per store. A good idea for maintaining
-    uniqueness here might be to take the cid of the customers table and the
-    current time and hash that as the txid. Attributes are:
-
-    txid = unique transaction identifier
-    sid = store id
-    amount = total for the transaction
-    '''
-
-    def __init__(self, txid, sid, amount):
-        self._dict = {'txid': txid, 'sid': sid, 'amount': amount}
-
-@saves_to_file('Data/users.csv')
-class user(DictLike):
-    fields = ('uid', 'username', 'password')
-
-    def __init__(self, uid, username, password):
-        self._dict = {'uid': uid, 'username': username, 'password': password}
-
-
-#================================= Generator ==================================#
+def random_choice_gen(seq):
+    while True:
+        yield random.choice(seq)
 
 # Generate random first name
 def fname_gen():
@@ -188,8 +34,7 @@ def fname_gen():
               'Brad','Steven','Charles','Ashley','John','James','Jacob','Mark',
               'Michael','Edward','Donald','Zachary','Sean','Blake','Jennifer',
               'Sarah','Yao','Brandon','Albert']
-    while True:
-        yield random.choice(fnames)
+    return random_choice_gen(fnames)
 
 
 # Generate random last name
@@ -198,8 +43,7 @@ def lname_gen():
     lnames = ['Johnson','Smith','Williams','Brown','Jackson','Ming','Zhang',
               'Jefferson','Thomas','Taylor','Moore','Loss','Davis','Garcia',
               'Miller','Jones','Wilson','Less']
-    while True:
-        yield random.choice(lnames)
+    return random_choice_gen(lnames)
 
 # Generate random decimal numbers
 def decimal_gen(min, max, precision):
@@ -219,16 +63,17 @@ def uname_gen():
               'magnumdragon','shroud','xgod','taz','mark','milkman',
               'masterblaster','thunderous','fireknight','shovelfighter']
 
-    while True:
-        yield random.choice(unames) + str(random.randint(10,100))
+    for name in random_choice_gen(unames):
+        yield name + str(random.randint(10,100))
 
 
 # Generate passwords
 def pass_gen():
     passwords = [ 'Hunter2','__Hunter2','Password','P@$$W0rd','Adm1n',
                   'SodiumB1c4rb0n4t3','Def4ult5']
-    while True:
-        yield bcrypt_sha256.hash(random.choice(passwords))
+    for password in random_choice_gen(passwords):
+        yield bcrypt_sha256.hash(password)
+
 
 # Generate random telephone number
 def telno_gen():
@@ -262,16 +107,14 @@ def city_gen():
     # A list of cities
     cities = ['Jacksonville','Tampa','New York City','Chicago','China',
               'Atlanta','San Diego', 'San Francisco', 'Carlsbad']
-    while True:
-        yield random.choice(cities)
+    return random_choice_gen(cities)
 
 
 # Generate random state
 def state_gen():
     # A list of States
     states = ['Florida','New York','Georgie','Illinois','California']
-    while True:
-        yield random.choice(states)
+    return random_choice_gen(states)
 
 def zip_gen():
     while True:
@@ -286,23 +129,29 @@ def pname_gen():
                  'Velcro strips','Rope','Chain (100 feet, 2-inch)','Chainsaw',
                  'Desert Eagle','Gerbil','Chinchilla','German Shepherd',
                  'Goat','Dynamit','Extension Cord (50 feet)']
-    while True:
-        yield random.choice(products)
+    return random_choice_gen(products)
 
 def color_gen():
     colors = ['Red','Blue','Green','White','Black','Brown','Orange','Yellow',
               'Purple','Pink','Teal','Maroon']
-    while True:
-        yield random.choice(colors)
-
+    return random_choice_gen(colors)
 
 #============================== Object Creation ===============================#
 
+# The following functions create n rows for the corresponding relation
+# They return a list of dicts that can be easily passed to csv.DictWriter.writerow
+# Some of the functions require results from a previous function (i.e. reference
+# to another relation)
+
 def make_roles(n):
+    fields = ('roleid', 'role')
     roles = ['Cashier', 'Manager', 'Stocker', 'Human Resources', 'Information Technology']
-    return [role(*args) for args in zip(range(1, n+1), roles)]
+    values = list(zip(range(1, n+1), roles))
+    return (fields, values)
 
 def make_employees(n, roles):
+    fields = ('eid', 'firstname', 'lastname', 'roleid', 'pay', 'hourly')
+
     fnames = fname_gen()
     lnames = lname_gen()
     wages = decimal_gen(10, 25, 2)
@@ -313,71 +162,205 @@ def make_employees(n, roles):
     for id in range(1, n+1):
         fname, lname, hourly = map(next, (fnames, lnames, bools))
         pay = next(wages) if hourly else next(salaries)
-        roleid = random.choice(roles)['roleid']
-        employees.append(employee(id, fname, lname, roleid, pay, hourly))
+        roleid = random.choice(roles)[0]
+        employees.append((id, fname, lname, roleid, pay, hourly))
 
-    return employees
+    return (fields, employees)
 
 def make_employment(n, employees, stores):
-    pass
+    fields = ('sid', 'eid')
+    employment_list = []
+    for _ in range(n):
+        sid = random.choice(stores)[0]
+        eid = random.choice(employees)[0]
+        employment_list.append((sid, eid))
+
+    return (fields, employment_list)
 
 def make_stores(n):
-    return [store(*args)
-            for args in zip(range(1, n+1), zip_gen(), address_gen(),
-                            city_gen(), state_gen(), telno_gen())]
+    '''make_stores(n) -> list of store dicts
+
+    The keys are:
+
+    sid = storeid, unique store identifier
+    zip = store zip code (assumes only US-based stores)
+    address = street address of the store
+    city = city the store is in
+    state = state the store is in
+    telno = telephone number of the store
+    '''
+
+    fields = ('sid', 'address', 'city', 'state', 'zip', 'telno')
+    values = list(zip(range(1, n+1), zip_gen(), address_gen(), city_gen(),
+                      state_gen(), telno_gen()))
+
+    return (fields, values)
 
 def make_products(n):
-    return [product(*args) for args in zip(range(1, n+1), pname_gen(), color_gen())]
+    '''make_product(n) -> list of product dicts
+
+    The keys are:
+
+    pid = product id, unique numeric value representing the product
+    name = product name, what the item is (e.g. 'xbox', 'ps3', 'hammer', 'etc').
+    color = color of the product. For our purposes we allow only a single color
+            per item
+    '''
+
+    fields = ('pid', 'name', 'color')
+    values = list(zip(range(1, n+1), pname_gen(), color_gen()))
+
+    return (fields, values)
 
 def make_inventory(n, stores, products):
-    pass
+    '''make_inventory(n) -> list of inventory dicts
+
+    These dicts contain the inventory information for any given store. It does
+    so by mapping store id's to product id's. The keys are as follows:
+
+    self.sid = store id
+    self.pid = product id
+    self.price = price of the product at a given store
+    self.qty = how many of that product a store has
+    self.special = whether the item is on special in a given store
+
+    '''
+    fields = ('sid', 'pid', 'price', 'qty', 'special')
+
+    price_gen = decimal_gen(10, 100, 2)
+    special_gen = bool_gen()
+    inventory = []
+    for _ in range(n):
+        sid = random.choice(stores)[0]
+        pid = random.choice(products)[0]
+        price = next(price_gen)
+        amount = random.randint(1, 1000)
+        special = next(special_gen)
+        inventory.append((sid, pid, price, amount, special))
+
+    return (fields, inventory)
 
 def make_transactions(n, stores, products):
-    pass
+    '''make_transactions(n, stores, products) -> list of transaction dicts
+
+    This holds records of transactions per store. A good idea for maintaining
+    uniqueness here might be to take the cid of the customers table and the
+    current time and hash that as the txid. Attributes are:
+
+    txid = unique transaction identifier
+    sid = store id
+    amount = total for the transaction
+    '''
+
+    fields = ('txid', 'sid', 'pid', 'cost', 'amount')
+
+    price_gen = decimal_gen(10, 100, 2)
+    transactions = []
+    for txid in range(1, n+1):
+        sid = random.choice(stores)[0]
+        pid = random.choice(products)[0]
+        cost = next(price_gen)
+        amount = random.randint(1, 10)
+        transactions.append((txid, sid, pid, cost, amount))
+
+    return (fields, transactions)
 
 def make_suppliers(n):
-    pass
+    '''make_suppliers(n) -> list of supplier dicts
 
-def make_supplies(n, products, suppliers):
-    pass
+    The keys are:
 
-def make_orders(n, products, stores, supplies):
-    pass
+    supid = supplier id. A numeric representation of who this supplier is
+    name = name of the supplier
+    '''
+    fields = ('supid', 'name')
+    names = ['We Sell Everything', 'Ants in My Eyes Johnson Electronics', 'Tools \'R\' Us']
+    values = list(zip(range(1, n+1), names))
+    return (fields, values)
+
+# def make_supplies(n, products, suppliers):
+#     fields = ('supid', 'pid', 'cost', 'qty')
+#     supplies = []
+#     price_gen = decimal_gen(1000, 100000, 2)
+#     for _ in range(n):
+#         supid = random.choice(suppliers)['supid']
+#         pid = random.choice(products)['pid']
+#         cost = next(price_gen)
+#         amount = random.randint(100, 1000)
+#         supplies.append((supid, pid, cost, qty))
+#
+#     return (fields, supplies)
+
+def make_orders(n, products, stores, suppliers):
+    fields = ('oid', 'sid', 'pid', 'num', 'cost')
+    orders = []
+    price_gen = decimal_gen(1000, 100000, 2)
+    for oid in range(1, n+1):
+        sid = random.choice(stores)[0]
+        pid = random.choice(products)[0]
+        num = random.randint(100, 1000)
+        cost = next(price_gen)
+        orders.append((oid, sid, pid, num, cost))
+
+    return (fields, orders)
 
 def make_users(n):
-    return [user(*args) for args in zip(range(1, n+1), uname_gen(), pass_gen())]
+    fields = ('uid', 'username', 'password')
+    values = list(zip(range(1, n+1), uname_gen(), pass_gen()))
+    return (fields, values)
 
 
 # Generate the actual CSV files
-def gen_rows(n=100):
+def write_csv_tables(n=100):
+    tables = {}
 
-    store_hearder = 'zip,address,city,telno'
-    product_header = 'name,color'
-    inventory_header = 'sid,pid,price,qty,special'
-    prod_supplier_header = 'txid,supid,pid,cost,qty'
-    employement_header = 'sid,eid'
-    employee_header = 'roleid,firstname,lastname,pay,hourly'
-    role_header = 'roleid,role'
+    print('Creating roles')
+    tables['roles'] = roles = make_roles(n)
 
-    all_objects = {}
+    print('Creating employees')
+    tables['employees'] = employees = make_employees(n, roles[1])
 
-    for path, gen, cls in _file_class_assocs:
-        all_objects[cls] = (path, [])
-        cur_list = all_objects[cls][1]
+    print('Creating stores')
+    tables['stores'] = stores = make_stores(n)
 
-        for _, obj in gen(n, all_objects):
-            cur_list.append(obj)
+    print('Creating employment')
+    tables['employment'] = employment = make_employment(n, employees[1], stores[1])
 
+    print('Creating products')
+    tables['products'] = products = make_products(n)
+
+    print('Creating inventory')
+    tables['inventory'] = inventory = make_inventory(n, stores[1], products[1])
+
+    print('Creating transactions')
+    tables['transactions'] = transactions = make_transactions(n, stores[1], products[1])
+
+    print('Creating suppliers')
+    tables['suppliers'] = suppliers = make_suppliers(n)
+
+    print('Creating orders')
+    tables['orders'] = orders = make_orders(n, products[1], stores[1], suppliers[1])
+
+    print('Creating users')
+    tables['users'] = users = make_users(n)
+
+    for tablename, table in tables.items():
+        path = os.path.join(THIS_FILE_PATH, 'Data', tablename + '.csv')
         with open(path, 'w') as f:
-            print(cls.fields)
-            writer = csv.DictWriter(f, fieldnames=cls.fields)
-            writer.writeheader()
-# =============== [ Main ] ============== #
-# Create the data directory if it does not exist already
-if not os.path.exists('Data'):
-    os.mkdir('Data')
-elif not os.path.isdir('Data'):
-    e = RuntimeError('Path %r exists but is not a directory' % 'Data')
-    raise e
+            print('Writing {} rows to {}'.format(len(table[1]), path))
+            writer = csv.writer(f)
+            writer.writerow(table[0])
+            writer.writerows(table[1])
 
-gen_rows(10)
+
+
+# =============== [ Main ] ============== #
+if __name__ == '__main__':
+    # Create the data directory if it does not exist already
+    if not os.path.exists('Data'):
+        os.mkdir('Data')
+    elif not os.path.isdir('Data'):
+        e = RuntimeError('Path %r exists but is not a directory' % 'Data')
+        raise e
+
+    write_csv_tables(100)
