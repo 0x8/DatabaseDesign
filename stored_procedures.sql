42a6c50ca0c04f9f8a2c9401b2f1121ad1bd2c4e
user.mail <nullp0inter>
2017-04-18 20:01:16 -0400

    Working functions for salary and hourly pay by state, city, and zip as well as a dynamic function to select all stores by state

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -4,31 +4,34 @@
 
 -- Create the language in which the procedures 
 -- are written in the database
-CREATE LANGUAGE 'plpgsql';
+-- This need only be run once ever so leave it commented if you've
+-- already done it.
+--CREATE LANGUAGE 'plpgsql';
 
 -- Compile the average salary for a given zip code
-CREATE OR REPLACE FUNCTION avg_sal_zip(zip TEXT) RETURNS FLOAT AS $$
+CREATE OR REPLACE FUNCTION avg_sal_zip (zip TEXT) RETURNS FLOAT AS $$
 DECLARE
-	sal_avg := 0;
+	sal_avg float := 0;
 BEGIN
 
-IF NOT EXISTS--A
-(SELECT 1
- FROM Stores S
- WHERE S.zip = zip)
-THEN RETURN -1.0;
+    IF NOT EXISTS--A
+        (SELECT 1
+        FROM Stores S
+        WHERE S.zip = $1)
+    THEN RETURN -1.0;
 
-ELSE
+    ELSE
 
-SELECT INTO sal_avg ROUND(AVG(E.pay), 2)
-FROM Employement emp, Employees E, Stores S
-WHERE emp.sid=S.sid AND S.zip=zip AND E.hourly=False
+        SELECT INTO sal_avg ROUND(AVG(E.pay), 2)
+        FROM Employment emp, Employees E, Stores S
+        WHERE emp.sid=S.sid AND S.zip=$1 
+              AND E.eid=emp.eid AND E.hourly='False';
 
-IF sal_avg IS NOT NULL--B
-THEN RETURN sal_avg;
-ELSE RETURN -1.0;
-END IF;--B
-END IF;--A
+        IF sal_avg IS NOT NULL--B
+            THEN RETURN sal_avg;
+            ELSE RETURN -1.0;
+        END IF;--B
+    END IF;--A
 END;
 $$ LANGUAGE plpgsql;
 
@@ -37,53 +40,129 @@ $$ LANGUAGE plpgsql;
 -- Compile the average hourly pay for a given zip code
 CREATE OR REPLACE FUNCTION avg_hourly_zip(zip TEXT) RETURNS FLOAT AS $$
 DECLARE
-	hourly_avg := 0;
+	hourly_avg float := 0;
 BEGIN
 
-IF NOT EXISTS
-(SELECT 1
- FROM Stores S
- WHERE S.zip = zip)
-THEN RETURN -1.0;
+    IF NOT EXISTS
+        (SELECT 1
+        FROM Stores S
+        WHERE S.zip = $1)
+    THEN RETURN -1.0;
 
-ELSE
+    ELSE
 
-SELECT INTO hourly_avg ROUND(AVG(E.pay),2)
-FROM Employment emp, Stores S, Employees E
-WHERE emp.sid = S.sid AND S.zip = zip AND E.hourly='TRUE'
+        SELECT INTO hourly_avg ROUND(AVG(E.pay),2)
+        FROM Employment emp, Stores S, Employees E
+        WHERE emp.sid = S.sid AND S.zip=$1 
+              AND emp.eid=E.eid AND E.hourly='TRUE';
 
-IF hourly_avg IS NOT NULL
-THEN RETURN hourly_avg;
-ELSE RETURN -1.0;
-END IF;
-END IF;
+        IF hourly_avg IS NOT NULL
+            THEN RETURN hourly_avg;
+            ELSE RETURN -1.0;
+        END IF;
+    END IF;
 END;
-$$ LANGUAG plpgsql;
+$$ LANGUAGE plpgsql;
 
 
 
 -- Get the average salary per store for all stores in a city
-CREATE OR REPLACE FUNCTION avg_salary(city TEXT) RETURNS FLOAT AS $$
+-- Returns -1.0 if the city is invalid, the average rounded to 2 places
+-- otherwise
+CREATE OR REPLACE FUNCTION avg_salary_city(city TEXT) RETURNS FLOAT AS $$
 DECLARE
-	city_sal_avg := 0;
+	city_sal_avg float := 0;
 BEGIN
 
-IF NOT EXISTS
-(SELECT 1
- FROM Stores S
- WHERE S.zip = zip)
-THEN RETURN -1.0;
+    IF NOT EXISTS
+        (SELECT 1
+        FROM Stores S
+        WHERE LOWER(S.city)=LOWER($1))
+    THEN RETURN -1.0;
 
-SELECT INTO city_sal_avg ROUND(AVG(E.pay),2)
-FROM Employment emp, Stores S, Employees E
-WHERE emp.sid = S.sid AND S.city = city AND E.hourly = 'FALSE'
+    ELSE
+
+        SELECT INTO city_sal_avg ROUND(AVG(E.pay),2)
+        FROM Employment emp, Stores S, Employees E
+        WHERE emp.sid = S.sid AND LOWER(S.city)=LOWER($1) 
+              AND emp.eid=E.eid AND E.hourly = 'FALSE';
+
+        IF city_sal_avg IS NOT NULL
+            THEN RETURN city_sal_avg;
+            ELSE RETURN -1.0;
+        END IF;
+    END IF;
+END;
+$$LANGUAGE plpgsql;
 
 -- Get the average hourly pay per store for all stroes in a city
+CREATE OR REPLACE FUNCTION avg_hourly_city(city TEXT) RETURNS FLOAT AS $$
+DECLARE
+    city_hourly_avg float := -1.0;
+BEGIN
+
+    IF NOT EXISTS
+        (SELECT 1
+        FROM Stores S
+        WHERE LOWER(S.city)=LOWER($1))
+    THEN RETURN -1.0;
+
+    ELSE
+
+        SELECT INTO city_hourly_avg ROUND(AVG(E.pay),2)
+        FROM Employment Emp, Stores S, Employees E
+        WHERE Emp.sid = S.sid AND LOWER(S.city)=LOWER($1) 
+              AND Emp.eid=E.eid AND E.hourly='True';
+
+        IF city_hourly_avg IS NOT NULL
+            THEN RETURN city_hourly_avg;
+            ELSE RETURN -1.0;
+        END IF;
+    END IF;
+END;
+$$LANGUAGE plpgsql;
+
 
 
 -- Get the average salary per store for all stores in a state
+CREATE OR REPLACE FUNCTION avg_salary_state(state TEXT) RETURNS FLOAT AS $$
+DECLARE
+    state_salary_avg float := -1.0;
+BEGIN
+
+    IF NOT EXISTS
+        (SELECT 1
+        FROM Stores S
+        WHERE LOWER(S.state)=LOWER($1))
+    THEN RETURN -1.0;
+    
+    ELSE 
+
+        SELECT INTO state_salary_avg ROUND(AVG(E.pay), 2)
+        FROM Stores S, Employees E, Employment Emp
+        WHERE Emp.sid=S.sid AND Emp.eid=E.eid AND LOWER(S.state)=LOWER($1)
+              AND E.Hourly='False';
+        
+        IF state_salary_avg IS NOT NULL
+            THEN RETURN state_salary_avg;
+            ELSE RETURN -1.0;
+        END IF;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
 
 
 -- Get the average hourly pay per store for all stores in a state
 
 -- Get the employees for a given store
+
+
+-- EXAMPLE OF DYNAMIC QUERY
+-- This one uses SQL as the language but can insert dynamically into args:
+CREATE OR REPLACE FUNCTION stores_in_state(state text) RETURNS
+SETOF stores AS $$
+    SELECT *
+    FROM Stores S
+    WHERE LOWER(S.state)=LOWER($1);
+$$ LANGUAGE 'sql' STABLE;
